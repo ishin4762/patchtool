d0497a5bfeb54eb87a354bd56cc319711216d2e4
ishin4762 <brokendesk0206@gmail.com>
2021-02-23 05:11:20 +0900

    fix bug.

--- src/gui/patchgen_gui/patchgen_gui_ja_JP.ts
+++ src/gui/patchgen_gui/patchgen_gui_ja_JP.ts
@@ -5,94 +5,102 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="35"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="126"/>
         <source>patchgen GUI ver. 0.2.0</source>
-        <oldsource>patchgen GUI</oldsource>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="48"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="127"/>
         <source>Patch file</source>
-        <translation>パッチファイル</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="60"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="128"/>
         <source>executable</source>
-        <translation>自己解凍形式</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="92"/>
-        <source>Output patchfile location:</source>
-        <translation>出力するパッチファイル:</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="119"/>
-        <source>Sources</source>
-        <translation>差分生成元データ</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="131"/>
-        <source>Older version directory:</source>
-        <translation>旧バージョンのディレクトリ:</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="177"/>
-        <source>Newer version directory:</source>
-        <translation>新バージョンのディレクトリ:</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="204"/>
-        <source>Create</source>
-        <translation>作成</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="220"/>
-        <source>Log:</source>
-        <translation>ログ:</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="245"/>
-        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS UI Gothic&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS UI Gothic&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="76"/>
         <location filename="mainwindow.ui" line="154"/>
         <location filename="mainwindow.ui" line="190"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="129"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="133"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="135"/>
         <source>...</source>
-        <translation>...</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="92"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="130"/>
+        <source>Output patchfile location:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="119"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="131"/>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="132"/>
+        <source>Older version directory:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="177"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="134"/>
+        <source>Newer version directory:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="204"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="136"/>
+        <source>Create</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="220"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="137"/>
+        <source>Log:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="245"/>
+        <location filename="../../../build/src/gui/patchgen_gui/patchgen_gui_autogen/include/ui_mainwindow.h" line="138"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS UI Gothic&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="24"/>
         <source>Choose patch file location</source>
-        <translation>パッチファイルの出力先の指定</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="37"/>
         <source>Choose older version&apos;s directory location</source>
-        <translation>旧バージョンのディレクトリ指定</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="50"/>
         <source>Choose new version&apos;s directory location</source>
-        <translation>新バージョンのディレクトリ指定</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="62"/>
         <source>Warning</source>
-        <translation>入力エラー</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="62"/>
         <source>Text boxes must be filled.</source>
-        <translation>テキストボックスを埋めてください。</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
